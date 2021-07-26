af9dd48166ea4486c891604cc0ea37f0c6042c36
Shirish9294 <37794995+Shirish9294@users.noreply.github.com>
2021-07-26 15:34:51 -0500

    Update settings.py

--- efs/settings.py
+++ efs/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_p=jyr+mxkgmk-^93mq1t(nb#xd%)wjt9*@m)g$k9)40q*zb=4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
 
 # Application definition
